3abbb6ef23406608f15b7ce4aaa7704cd08d6cd4
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-12-14 16:20:00 -0300

    feat: add test crud

---
+++ src/users/controller/users.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from '../service/users.service';
+import { UserDTO } from '../dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() data: UserDTO) {
+    return await this.usersService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UserDTO) {
+    return await this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.usersService.remove(id);
+  }
+}
